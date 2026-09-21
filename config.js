56974c74d734c624028f9bf0febad191b5bce347
anthonyzhu1130 <zhg.123@163.com>
2026-09-21 13:48:33 +0800

    Add quick links to config.js

--- config.js
+++ config.js
@@ -47,4 +47,10 @@ window.APP_CONFIG = {
 
   /* 是否显示示例数据提示（true / false） */
   SHOW_SAMPLE_DATA: false
+    QUICK_LINKS: [
+    { name: '百度', url: 'https://www.baidu.com', icon: '🔍' },
+    { name: '光聆导航', url: 'https://anthonyzhu1130.github.io/nav/#/', icon: '🐙' },
+    { name: '知乎', url: 'https://www.zhihu.com', icon: '📚' },
+    { name: '虫部落', url: 'https://search.chongbuluo.com/', icon: '📺' }
+  ]
 };
